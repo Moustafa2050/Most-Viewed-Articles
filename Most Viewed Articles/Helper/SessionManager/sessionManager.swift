64ce04cd576a4956d6b23519427af6c701093171
Moustafa60 <moustafa.joumaaalrajab.ext@vianeos.com>
2023-04-29 19:54:27 +0400

    add NetworkReachabilityManager to session Manager

--- Most Viewed Articles/Helper/SessionManager/sessionManager.swift
+++ Most Viewed Articles/Helper/SessionManager/sessionManager.swift
@@ -11,16 +11,24 @@ import Alamofire
 
 class sessionManager {
     static let shared = sessionManager()
-
+    
     private let session: Session
-
+    private let reachabilityManager = NetworkReachabilityManager()
     private init() {
         let configuration = URLSessionConfiguration.default
         configuration.timeoutIntervalForRequest = 30
         session = Session(configuration: configuration)
     }
-
+    func isConnectedToInternet() -> Bool {
+        return reachabilityManager?.isReachable ?? false
+    }
+    
     func get(url: URLConstants, parameters: Parameters? = nil, completion: @escaping (Result<Data, Error>) -> Void) {
+        if !isConnectedToInternet() {
+            let error = NSError(domain: "Internet connection lost", code: -1009, userInfo: nil)
+            completion(.failure(error))
+            return
+        }
         session.request(url.rawValue, method: .get, parameters: parameters)
             .validate()
             .responseData { response in
@@ -32,8 +40,13 @@ class sessionManager {
                 }
             }
     }
-
+    
     func post(url: String, parameters: Parameters? = nil, completion: @escaping (Result<Data, Error>) -> Void) {
+        if !isConnectedToInternet() {
+            let error = NSError(domain: "Internet connection lost", code: -1009, userInfo: nil)
+            completion(.failure(error))
+            return
+        }
         session.request(url, method: .post, parameters: parameters)
             .validate()
             .responseData { response in
